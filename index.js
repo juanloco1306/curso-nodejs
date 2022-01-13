c866c44eea690a0dc5ce13ce90d210fac75ed38d
juanloco1306 <juanloco1306@gmail.com>
2022-01-13 17:12:13 -0600

    Módulos preconstruidos en Nodejs

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const math = require('./math.js');
+const os = require('os');
 
-console.log(math);
-/*console.log(math.add(5,6));
-console.log(math.substract(13,9));
-console.log(math.multiply(7,10));
-console.log(math.divide(9,3));
-console.log(math.divide(9,0));
-*/
+console.log(os.platform());
