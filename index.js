9036581801070b91fa3c7d2e1ab92deb13de70bc
juanloco1306 <juanloco1306@gmail.com>
2022-01-13 17:02:44 -0600

    explicacion de como sirven los modulos

---
+++ index.js
@@ -0,0 +1,9 @@
+const math = require('./math.js');
+
+console.log(math);
+/*console.log(math.add(5,6));
+console.log(math.substract(13,9));
+console.log(math.multiply(7,10));
+console.log(math.divide(9,3));
+console.log(math.divide(9,0));
+*/
