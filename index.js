a4ff4aafa8018579f960663868070bec21ce0204
juanloco1306 <juanloco1306@gmail.com>
2022-01-13 20:20:24 -0600

    creamos un servidor

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('./texto.txt', function (err, data) {
-    if (err){
-        console.log(err);
-    }
-    console.log(data.toString());
+/*http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+}).listen(3000);*/
+
+
+// Tambien se puede escribir asi
+/*const handleServer = function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+}
+
+http.createServer(handleServer).listen(3000);
+*/
+
+const handleServer = function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.write('<h1>Hola Mundo</h1>');
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function () {
+    console.log('Server on port 3000');
 });
