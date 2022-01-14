3f7e30a58c8c926d44f8266aea826dc81255ad4b
juanloco1306 <juanloco1306@gmail.com>
2022-01-13 20:44:48 -0600

    aprendemos como utilizar npm y los modulos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+const colors = require('colors');
+
 /*http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-type': 'text/html' });
     res.write('<h1>Hola Mundo</h1>');
@@ -26,5 +28,5 @@ const handleServer = function (req, res) {
 const server = http.createServer(handleServer);
 
 server.listen(3000, function () {
-    console.log('Server on port 3000');
+    console.log('Server on port 3000'.rainbow);
 });
