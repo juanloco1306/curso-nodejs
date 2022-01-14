3a706ab4d7570e83ef0fa0d953dd93c6d7e624a5
juanloco1306 <juanloco1306@gmail.com>
2022-01-13 19:46:30 -0600

    se le lo que dice el texto

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 
-fs.writeFile('./texto.txt', 'linea uno', function (err) {
-    if (err) {
+fs.readFile('./texto.txt', function (err, data) {
+    if (err){
         console.log(err);
     }
-    console.log('Archivo creado');
+    console.log(data.toString());
 });
-
-console.log('ultima linea de codigo');
